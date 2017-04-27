ba37044829fb3edd198ed759270bb43029e17363
Sophie Hsu <fidlar@ip210-177.wlan.ntust.edu.tw>
2017-04-27 18:37:34 +0800

    add adjust vector to same length

--- NumberObject.cpp
+++ NumberObject.cpp
@@ -1,15 +1,26 @@
 #include "NumberObject.h"
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 vector<int> NumberObject::add(vector<int> num1, vector<int> num2) {
 	vector<int> result;		//a vector to sort results
 	int ten = 0;
     
-	//reverse the vector to calculate from the ones
+    //set both vector to the same length
+    double gapSize = num1.size() - num2.size();
+    if(gapSize > 0.0){
+        num2.insert(num2.begin(), (int)abs(gapSize), 0);
+    }
+    if (gapSize < 0.0){
+        num1.insert(num1.begin(), (int)abs(gapSize), 0);
+    }
+	
+    //reverse the vector to calculate from the ones
 	reverse(num1.begin(), num1.end());
 	reverse(num2.begin(), num2.end());
-
+    
+    //start addition
 	for (int i = 0; i < num1.size(); i++) {		//calculate one number at a time
 		int sum = num1[i] + num2[i] + ten;		//add the ten that was left from last times calculation
 		ten = sum / 10;		//save ten for later 
@@ -24,6 +35,15 @@ vector<int> NumberObject::add(vector<int> num1, vector<int> num2) {
 vector<int> NumberObject::sub(vector<int> num1, vector<int> num2) {
 	vector<int> result;
 	int ten = 0;	//ten is whether the previous calculation had minus off one from the next tens
+
+    //set both vector to the same length
+    double gapSize = num1.size() - num2.size();
+    if(gapSize > 0.0){
+        num2.insert(num2.begin(), (int)abs(gapSize), 0);
+    }
+    if (gapSize < 0.0){
+        num1.insert(num1.begin(), (int)abs(gapSize), 0);
+    }
 
 	//reverse the vector to calculate from the ones
 	reverse(num1.begin(), num1.end());
